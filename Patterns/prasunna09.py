76412c46fff1397d5b7fa4ddf88b32227002a358
Prasunna Soppa <70575890+prasunna09@users.noreply.github.com>
2021-10-03 12:08:06 +0530

    Create prasunna09.py

---
+++ Patterns/prasunna09.py
@@ -0,0 +1,22 @@
+'''
+prints the diamond shape with user input
+'''
+n = int(input())
+
+for i in range(n):
+    for j in range(i, n-1):
+        print(" ", end = '')
+    for k in range(i+1):
+        print("* ", end = '')
+
+    print()
+    
+for i in range(n):
+    for j in range(i):
+        print(" ", end = '')
+    
+    for j in range(i, n):
+        print("* ", end= '')    
+    
+
+    print()
